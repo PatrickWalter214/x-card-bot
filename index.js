d27601d7728705af0dbdda35ab8690103e42c9dc
PatrickWalter214 <patrick.walter214@gmail.com>
2022-05-20 21:06:09 +0200

    Direct message handler

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.DIRECT_MESSAGES], partials: ['CHANNEL'] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+// Handle receiving a direct message
+client.on('messageCreate', message => {
+	console.log(message.author.id);
 });
 
 // Login to Discord with your client's token
