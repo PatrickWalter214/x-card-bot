a581a793e3c5c8210d60b0d44fabb8f8da49c717
PatrickWalter214 <patrick.walter214@gmail.com>
2022-05-27 20:45:44 +0200

    Fetch voice channel from user id

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.DIRECT_MESSAGES], partials: ['CHANNEL'] });
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES],
+	partials: ['CHANNEL'],
+});
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('x-card-bot is ready!');
 });
 
 // Handle receiving a direct message
 client.on('messageCreate', message => {
-	console.log(message.author.id);
+	const memberCallback = member => console.log(member.voice.channelId);
+	const guildCallback = guild => guild.members.fetch(message.author.id).then(memberCallback);
+	const guildsCallback = guilds => guilds.forEach(guild => client.guilds.fetch(guild.id).then(guildCallback));
+	client.guilds.fetch().then(guildsCallback);
 });
 
 // Login to Discord with your client's token
