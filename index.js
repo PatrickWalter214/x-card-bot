481cd74aa6b0092c0f7e86347034c7895ae0cea3
PatrickWalter214 <patrick.walter214@gmail.com>
2022-06-06 00:02:37 +0200

    Play audio file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Require the necessary discord.js classes
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { generateDependencyReport, joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, audio } = require('./config.json');
+const { join } = require('node:path');
 
 // Create a new client instance
 const client = new Client({
@@ -11,28 +12,35 @@ const client = new Client({
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
+	console.log(generateDependencyReport());
 	console.log('x-card-bot is ready!');
 });
 
 // Handle receiving a direct message
 client.on('messageCreate', message => {
-	const memberCallback = member => {
-		if (member.voice.channelId != null) {
-			const connection = joinVoiceChannel({
-				channelId: member.voice.channelId,
-				guildId: member.voice.guild.id,
-				adapterCreator: member.voice.guild.voiceAdapterCreator,
-			});
-			setTimeout(() => connection.destroy(), 1000);
-		}
-		else {
-			console.log('No voice channel detected');
-		}
-	};
+	const memberCallback = member => joinAndPlayAudio(member.voice);
 	const guildCallback = guild => guild.members.fetch(message.author.id).then(memberCallback);
 	const guildsCallback = guilds => guilds.forEach(guild => client.guilds.fetch(guild.id).then(guildCallback));
 	client.guilds.fetch().then(guildsCallback);
 });
+
+const joinAndPlayAudio = voice => {
+	if (voice.channelId != null) {
+		const connection = joinVoiceChannel({
+			channelId: voice.channelId,
+			guildId: voice.guild.id,
+			adapterCreator: voice.guild.voiceAdapterCreator,
+		});
+		const player = createAudioPlayer();
+		player.on(AudioPlayerStatus.Idle, () => {
+			player.stop();
+			connection.destroy();
+		});
+		connection.subscribe(player);
+		const resource = createAudioResource(join(__dirname, 'audio', audio));
+		player.play(resource);
+	}
+};
 
 // Login to Discord with your client's token
 client.login(token);
